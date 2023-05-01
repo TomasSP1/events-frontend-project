b6d197c3077edff2b5b78261bab8a5d4f7b8cd9d
TomasSP1 <fobas0405@gmail.com>
2023-05-01 16:50:40 +0300

    new registration event form

---
+++ src/services/CategoriesService.js
@@ -0,0 +1,23 @@
+
+//su axios biblioteka kreiptis į savo suskurtą API
+// import axios from 'axios';
+
+// const API_URL = 'https://events-80pg.onrender.com/api/categories';
+
+// //get categories
+// const getCategories = async() => {
+
+//     try {
+//         const response = await axios.get(API_URL);
+//         //console.log(response);
+//         return response;
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
+
+
+
+// const categoriesService = { getCategories }
+
+// export default categoriesService
