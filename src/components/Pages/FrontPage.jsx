758245a0886ec2b3a18e41cc6b884b17910603aa
TomasSP1 <fobas0405@gmail.com>
2023-05-03 15:15:18 +0300

    veikiantis reg event form ir pagal ideja turetu iskart atvaizduoti

--- src/components/Pages/FrontPage.jsx
+++ src/components/Pages/FrontPage.jsx
@@ -4,18 +4,18 @@ import { Card, Col, Container, Row } from "react-bootstrap";
 import "../CSS/FrontPage.css";
 import eventServices from "../../services/eventsServices";
 import EventCard from "./EventCard";
+import EventRegForm from "./EventRegForm";
 
 function FrontPage() {
   const [events, setEvents] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState(null);
 
+  const getEventsData = async () => {
+    const data = await eventServices.getEvents();
+    setEvents(data);
+  };
+
   useEffect(() => {
-    const getEventsData = async () => {
-      const data = await eventServices.getEvents();
-
-      setEvents(data);
-    };
-
     getEventsData();
   }, []);
 
@@ -34,23 +34,19 @@ function FrontPage() {
           />
         </Col>
         <Col className="mt-5" md={10}>
-          <div className="card-row d-flex align-items-center flex-wrap">
-            {events
-              .filter(
-                (event) =>
-                  !selectedCategory || event.category === selectedCategory
-              )
-              .map((event) => (
-                <EventCard
-                  key={event._id}
-                  eventImage={event.image}
-                  eventTitle={event.title}
-                  eventCategory={event.category}
-                  eventDate={event.date}
-                  eventDescription={event.description}
-                  eventPlace={event.place}
-                />
-              ))}
+          <EventRegForm getEventsData={getEventsData}/>
+          <div className="card-row d-flex align-items-center justify-content-center flex-wrap my-5">
+            {filteredEvents.map((event) => (
+              <EventCard
+                key={event._id}
+                eventImage={event.image}
+                eventTitle={event.title}
+                eventCategory={event.category}
+                eventDate={event.date}
+                eventDescription={event.description}
+                eventPlace={event.place}
+              />
+            ))}
           </div>
         </Col>
       </Row>
